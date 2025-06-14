f564476a1f665cf8463d83f3eb39d4ffc9169040
José Fausto <fau12.martinsmelo@gmail.com>
2025-06-14 00:09:14 -0300

    teste

---
+++ Data e hora/Datetime.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime, time
+
+data=(2023, 10, 7)
+print(data)
+print(date.today())
+
+data_hora = datetime(2023, 7, 10)
+print(data)
+print(datetime.today())
+
+hora = time (10, 20, 0)
+print(hora)
+
+
---
+++ Data e hora/timedelta.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime, timedelta
+
+tipo_carro = "M" # P, M, G,
+tempo_pequeno = 30
+tempo_medio = 45
+tempo_grande = 60
+data_atual = datetime.now()
+
+if tipo_carro == "p":
+    data_estimada = data_atual - timedelta(days=tempo_pequeno)
+    print(f"O carro chegou: {data_atual} e ficará pronto ás {data_estimada}")
+elif tipo_carro == "M":
+    data_estimada = data_atual - timedelta(days=tempo_medio)
+    print(f"O carro chegou: {data_atual} e ficará pronto ás {data_estimada}")
+else:
+    data_estimada = data_atual - timedelta(days=tempo_grande)
+    print(f"O carro chegou: {data_atual} e ficará pronto ás {data_estimada}")        
+
+print(date.today() - timedelta(days=1))
+
+resultado =datetime(2023, 7, 25, 10, 19, 20) - timedelta(hours=1)
+print(resultado.time())
+
+print(datetime.now().date())
